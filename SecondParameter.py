56ae0f579af8af46ab3f70f417b27b33f2d47c9e
aonanam <aonanam@bu.edu>
2015-12-04 11:30:13 -0500

    Outline for Distance

--- SecondParameter.py
+++ SecondParameter.py
@@ -1,2 +1,15 @@
 #here is where the code to properly identify the second parameter
 #DISTANCE, will be found from probabilities based on training data
+
+def main():
+return
+
+def readCSV():	#taking in CSV data
+return
+
+def binData():	#putting data into bins by distance -- should use binSize = 250
+
+def gaussianize(): #makes binned data Gaussian -- should use gaussian library?
+return
+
+
