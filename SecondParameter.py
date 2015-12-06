7602dbdf6f699249bdd118d025eed7411ca5e195
aonanam <aonanam@bu.edu>
2015-12-06 16:09:36 -0500

    Distance CVS reading

--- SecondParameter.py
+++ SecondParameter.py
@@ -1,10 +1,34 @@
 #here is where the code to properly identify the second parameter
 #DISTANCE, will be found from probabilities based on training data
+'''pseudocode
+create gaussian based on the model data:
+ 
 
-def main():
+calculate probability:
+for every tf:
+	for every gene:
+		=tf/gene pair take their line numbers and calculate distance
+'''
+START_COLUMN = start 
+STOP_COLUMN = STOP_COLUMN	#up to whoever creates the csv
+BIN_SIZE = 250
+
+def SecondParam():
+
 return
 
-def readCSV():	#taking in CSV data
+def readCSV(lineNum, csvFile):	#taking in CSV data
+csvFile = open(csvFile)
+temp = []
+geneCount = 0
+for line in csvFile:
+	temp.append(line)
+	if lineNum == geneCount:
+		start = temp[geneCount][START_COLUMN]
+		stop = temp[geneCount][STOP_COLUMN]
+		return start, stop
+	geneCount+=1
+
 return
 
 def binData():	#putting data into bins by distance -- should use binSize = 250
