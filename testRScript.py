f043c37b2c9df380707f2330220d6b1c63c781e9
anero17 <anero17@bu.edu>
2015-12-07 18:22:08 -0500

    commit

--- testRScript.py
+++ testRScript.py
@@ -1,2 +1,4 @@
 import subprocess
 
+code = subprocess.check_call("Rscript helloworld.R", shell=True)
+#code is the error message that the command prompt/terminal will return. it will be a 0 unless there is an error.
