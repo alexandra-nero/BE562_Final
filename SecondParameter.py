d3fa1e977f92b2ac0b608be95d925c0d564933a0
aonanam <aonanam@bu.edu>
2015-12-06 19:54:00 -0500

    distance counting and started binning

--- SecondParameter.py
+++ SecondParameter.py
@@ -16,26 +16,43 @@ BIN_SIZE = 250
 TF_POS = 0
 GENE_POS = 0
 REG_GENES_POS = 1
-pairFileName = 'nameOfPairFile.csv'
-genesFileName = 'nameOfGenesFile.csv'
 
-#test
+def readTrainingDistances(genomeLength, pairFileName, genesFileName):
+	"returns a list of distances from all TFs to all the genes they regulate given the genome's length, and the names of both the TF/Promoter pair file and the all genes file"
+	trainingDistances = []
+	genesFile = open(genesFileName)
+	pairFile = open(pairFileName,'rb')
+	for tf in pairFile:				#looking through all TFs
+		tfPos = None
+		tfName = tf[TF_POS]
+		for gene in genesFile:		#looking for TF in gene file
+			if gene[GENE_POS] == tfName:
+				tfPos = gene[START_COLUMN]
+				if tfPos == None:
+					print ("ERROR: cannot read in position of TF")
+					break
+		if tfPos != None:
+			regGenes = tf[REG_GENES_POS]
+			regGenesList = []
+			regGenesList = regGenes.split()
+			pairDistances = []
+			for regGene in xrange len(regGenesList):	#looping through all genes regulated
+				regGeneName = regGenesList[regGene]
+				for gene in genesFile:					#looking for gene regulated by TF in gene file
+					if gene[GENE_POS] == regGeneName:
+						regGenePos=gene[START_COLUMN]
+						distance = []
+						distance.append(abs(regGenePos-tfPos))
+						distance.append(abs(distance[]-(genomeLength-1))
+						trainingDistances.append(min(distance))
+	return trainingDistances
 
-def SecondParam():
-genesFile = open(genesFileName)
-pairFile = open(pairFileName,'rb')
-for tf in pairFile:
-	tfPosition = 0
-	tfName = tf[TF_POS]
-	for gene in genesFile:
-		if gene[GENE_POS] == tfName:
-			tfPosition = gene[START_COLUMN]
-	regGenes = tf[1]
-	regGenesList = []
-	regGenesList = regGenes.split()
-	for 
-
-return
+def binTrainingDistances(genomeLength, trainingDistances):
+	"counts frequencies for distances in each bin given the length of the genome and the list of distances between the TFs and the promoters they regulate"
+	numberOfBins = (genomeLength/250)+1
+	bin = [numberOfBins]
+	for i in xrange len(trainingDistances)
+		correctBin = ((int(trainingDistances[i]-1)/250))
 
 def readCSV(lineNum, csvFile):	#taking in CSV data
 csvFile = open(csvFile)
