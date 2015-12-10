97c7b89b8bcb04198530fa94275d5bfd2a88b05b
aonanam <aonanam@bu.edu>
2015-12-10 00:33:32 -0500

    debugging

--- SecondParameter.py
+++ SecondParameter.py
@@ -23,14 +23,15 @@ BIN_SIZE = 250
 
 def readPosTrainingDistances(genomeLength, pairFileName, genesFileName):
 	"returns a list of distances from all TFs to all the genes they regulate given the genome's length, and the names of both the TF/Promoter pair file and the all genes file"
+	print("Starting to read in Training Distances")
 	posTrainDist = []
 	genesFile = open(genesFileName)
 	pairFile = open(pairFileName,'rb')
 	for tf in pairFile:				#looking through all TFs
 		tfPos = None
-		tfName = tf[TF_POS]
+		tfName = tf[TFFILE_TF_POS]
 		for gene in genesFile:		#looking for TF in gene file
-			if gene[GENE_POS] == tfName:
+			if gene[GENEFILE_GENE_POS] == tfName:
 				tfPos = gene[GENEFILE_START_COLUMN]
 				if tfPos == None:
 					print ("ERROR: cannot read in position of TF")
@@ -40,21 +41,21 @@ def readPosTrainingDistances(genomeLength, pairFileName, genesFileName):
 			regGenesList = []
 			regGenesList = regGenes.split()
 			posPairDist = []
-			for regGene in xrange len(regGenesList):	#looping through all genes regulated
+			for regGene in xrange(len(regGenesList)):	#looping through all genes regulated
 				regGeneName = regGenesList[regGene]
 				for gene in genesFile:					#looking for gene regulated by TF in gene file
-					if gene[GENE_POS] == regGeneName:
+					if gene[GENEFILE_GENE_POS] == regGeneName:
 						regGenePos=gene[GENEFILE_START_COLUMN]
 						posDist = []
 						posDist.append(abs(regGenePos-tfPos))
-						posDist.append(abs(posDist[0]-genomeLength)
+						posDist.append(abs(posDist[0]-genomeLength))
 						posTrainDist.append(min(posDist))
 	enzymeMatrix = []
 	negTrainDist = []
 	for gene in genesFile:
 		if gene[1] == 'F':
 			enzymeMatrix.append(gene)
-	for outerEnzyme in xrange len(enzymeMatrix):
+	for outerEnzyme in xrange(len(enzymeMatrix)):
 		outerPos = outerEnzyme[GENEFILE_GENE_POS]
 		for innerEnzyme in range(outerEnzyme, len(enzymeMatrix)):
 			negDist = []
@@ -62,7 +63,9 @@ def readPosTrainingDistances(genomeLength, pairFileName, genesFileName):
 			negDist.append(abs(outerPos - innerPos))
 			negDist.append(abs(negDist[0]-genomeLength))
 			negTrainDist.append(min(negDist))
+	print("Normalizing positive training distances")
 	posNormTrainDist = normTrainingDistances(genomeLength, posTrainDist)
+	print("Normalizing negative training distances")
 	negNormTrainDist = normTrainingDistances(genomeLength, negTrainDist)
 	return (posNormTrainDist, negNormTrainDist)
 
@@ -71,28 +74,37 @@ def normTrainingDistances(genomeLength, trainingDistances):
 	"counts frequencies for distances in each bin given the length of the genome and the list of distances between the TFs and the promoters they regulate, then normalizes them into probabilities"
 	numberOfBins = (genomeLength/250)+1
 	binnedDistances = [numberOfBins]
-	totalFreq==0
-	for i in xrange len(trainingDistances)
+	totalFreq=0
+	for i in xrange(len(trainingDistances)):
 		correctBin = ((int(trainingDistances[i]-1)/250))
 		binnedDistances[correctBin]+=1
 		totalFreq+=1
-	normDist = [x/totalFreq for x in binnedDistances]
-	print("Total frequency is: " totalFreq)
+	normDist=[]
+	if totalFreq!=0:
+		normDist = [x/totalFreq for x in binnedDistances]
+		print("Total frequency is: ", totalFreq)
+
 	return normDist
 
 def secondParamMain(genomeLength, rootFileName):
 	"manages the running of all other functions in second Parameter File"
 	genesFileName = str(rootFileName)+"Master.csv"
 	pairFileName = str(rootFileName)+"TFPairs.csv"
+	posNegTup = readPosTrainingDistances(genomeLength, pairFileName, genesFileName) 
+	
+	outputCSV(posNegTup[0], posNegTup[1], rootFileName)
+
 
 def outputCSV(posNormTrainDist, negNormTrainDist, rootFileName):
 	"creates a CSV file from the normalized data. requires the yes and no normalized probability lists"
+	print("Starting to write to CSV!")
 	secondParamCSV = csv.writer(open(""+rootFileName+"secondParam.csv", "wb"))
 	titleRow = ["Position (250 bp)","P(pair)","P(not pair)"]
 	secondParamCSV.writerow(titleRow)
-	for x in xrange(posNormTrainDist):
+	for x in xrange(len(posNormTrainDist)):
 		row = []
 		row.append(x)
 		row.append(posNormTrainDist)
 		row.append(negNormTrainDist)
 		secondParamCSV.writerow(row)
+secondParamMain(4639675, "Ecoli_MG1655")
