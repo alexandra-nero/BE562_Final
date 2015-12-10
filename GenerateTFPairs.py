a4f6b631823678396f72e26024c878cf416f9a92
anero17 <anero17@bu.edu>
2015-12-09 22:47:38 -0500

    commit

--- GenerateTFPairs.py
+++ GenerateTFPairs.py
@@ -9,29 +9,32 @@ def validGene(gene, fileName):
 
 
 def readCSVFile(fileName):
-	myFile = csv.reader(open(fileName+'TFPairs.csv', 'rb'))
+	myFile = csv.reader(open(fileName+'OldTFPairs.csv', 'rb'))
 	tfNum = 0
 	for row in myFile:
 		tfNum+=1
 	TFPairs = []
 	for i in xrange(tfNum):
 		TFPairs+=[[0]*2]
-	myFile2 = csv.reader(open(fileName+'TFPairs.csv', 'rb'))
+	myFile2 = csv.reader(open(fileName+'OldTFPairs.csv', 'rb'))
 	geneCount = 0
 	for row in myFile2:
 		tfString = row[0]
 		slashCount = 0
 		tfPiece = ""
+		index = 0
 		for c in tfString:
 			if (slashCount % 2 == 0 and c == '/'):
-				slashCount += 1
-				tfPiece += " "
+				if (tfString[index+1] == '/'):
+					slashCount += 1
+					tfPiece += " "
 			elif c == '/':
 				slashCount += 1
 			elif c == ' ':
 				continue
 			else:
 				tfPiece += c
+			index+=1
 
 		geneString = row[1]
 		slashCount = 0
@@ -51,32 +54,37 @@ def readCSVFile(fileName):
 		geneVector.append(genePiece)
 		index = 0
 		finalgeneString = ""
+		removeG = []
 		for gene in geneVector:
 			geneLower = gene[0].lower() + gene[1:]
 			if 'sup' in gene or 'sub' in gene or 'SUP' in gene or 'SUB' in gene:
-				geneVector.remove(gene)
-			elif validGene(gene[:5], fileName) or validGene(geneLower[:5], fileName):
+				removeG.append(gene)
+			elif validGene(gene[:5]+ " ", fileName) or validGene(geneLower[:5]+ " ", fileName):
 				finalgeneString += gene[:5] + " "
-			elif validGene(gene[:4], fileName) or validGene(geneLower[:4], fileName):
+			elif validGene(gene[:4] + " ", fileName) or validGene(geneLower[:4] + " ", fileName):
 				finalgeneString += gene[:4] + " "
-			elif validGene(gene[:3], fileName) or validGene(geneLower[:3], fileName):
+			elif validGene(gene[:3] + " ", fileName) or validGene(geneLower[:3] + " ", fileName):
 				finalgeneString += gene[:3] + " "
 			else:
-				geneVector.remove(gene)
-		
-		if(len(geneVector) > 0):
+				removeG.append(gene)
+
+		for element in removeG:
+			geneVector.remove(element)
+		newGeneVector = []
+		for i in geneVector:
+			if i not in newGeneVector:
+				newGeneVector.append(i)
+
+		if(len(newGeneVector) > 0):
 			TFPairs[geneCount][0] = tfPiece
 			TFPairs[geneCount][1] = finalgeneString
 			geneCount += 1
 	return TFPairs
 			
-
-
-		
-
 def createTFPairsFile(fileName):
 	TFPairs = readCSVFile(fileName)
-	cFile = csv.writer(open(fileName+"NewTFPairs.csv", 'wb'))
+	cFile = csv.writer(open(fileName+"TFPairs.csv", 'wb'))
 	for i in xrange(len(TFPairs)):
-		cFile.writerow(TFPairs[i])
+		if TFPairs[i][0] != 0 and TFPairs[i][1] != 0:
+			cFile.writerow(TFPairs[i])
 
