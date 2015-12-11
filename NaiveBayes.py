56ddb72bd41b9863c51fcd2f2c966cd7de6a2d72
anero17 <anero17@bu.edu>
2015-12-10 23:04:02 -0500

    commti

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,9 +1,10 @@
-#import FirstParameter
-#import SecondParameter
-#import ThirdParameter
+import FirstParameter
+import SecondParameter
+import ThirdParameter
 import GenerateTFPairs
 import MasterCSVCreator
 import csv
+import math
 
 #define all file Names here:
 fileName ='Ecoli_MG1655'
@@ -26,19 +27,58 @@ def splitData(fileName):
 		count += 1
 
 
-#def calculateAccuracy():
+def calculateAccuracy(ProbMatrix):
+	testFile = csv.writer(open(fileName+'TestTFPairs.csv', 'wb'))
+	for pair in trainFile:
+		regGene = pair[0]
+		tfGene = pair[1]
+		for row in ProbMatrix:
+			for column in ProbMatrix:
+
 
 
 
 #main function for entire program
 
 def runNaiveBayes():
-	finalFile = csv.writer(open(fileName+'PairProbability.csv', 'wb'))
+	finalProb = csv.reader(open(fileName+'FinalProb.csv', 'wb'))
+	finalMatrix = [] 
 	MasterCSVCreator.createCSV(fileName, geneNumber)
-	masterFile = 
+	masterTFFile = csv.reader(open(fileName+'Master.csv', 'rb'))
 	GenerateTFPairs.createTFPairsFile(fileName)
 	splitData(fileName)
-	for tfgene in 
+
+	#generate files from parameters
+	SecondParameter.secondParamMain(geneLength, fileName)
+	secondParamFile = csv.reader(open(fileName+"secondParam.csv", 'rb'))
+
+	pairCount = 0
+	for tfGene in masterTFFile:
+		if (tfGene[MasterCSVCreator.TF_COLUMN] == 'T' or tfGene[MasterCSVCreator.TF_COLUMN] == 'unknown'):
+			startTF = tfGene[MasterCSVCreator.START_COLUMN]
+			masterRegFile = csv.reader(open(fileName+'Master.csv', 'rb'))
+			for regGene in masterRegFile:
+				startReg = tf[MasterCSVCreator.START_COLUMN]
+				difference = abs(startTF-startReg)
+				wrapAround = abs(geneLength-difference)
+				totalDistance = min(difference, wrapAround)
+				currentBin = totalDistance/SecondParameter.BIN_SIZE
+
+				#first here
+				secondProbabilities = secondParamFile[currentBin]
+				thirdProbabilities = ThirdParameter.ThirdParam(tfGene, regGene)
+
+		NaiveBayes = (firstProbabilities[0]+secondProbabilities[0]+thirdProbabilities[0])/(firstProbabilities[1]+secondProbabilities[1]+thirdProbabilities[1])
+		NaiveBayesProb = math.log(NaiveBayes)
+		finalRow = [tfGene, regGene, NaiveBayesProb]
+		finalMatrix[pairCount][0] = tfGene
+		finalMatrix[pairCount][1] = regGene
+		finalMatrix[pairCount][2] = NaiveBayesProb
+		finalProb.writerow(finalRow)
+		print (calculateAccuracy(finalMatrix))
+
+
+
 
 
 main()	
