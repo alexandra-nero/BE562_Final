fb0c4a308edb25428e06a8dbd1e2c19a2be5acee
Shannon <arorascorpa@gmail.com>
2015-12-10 20:27:38 -0700

    Update pfinder3.py

--- pfinder3.py
+++ pfinder3.py
@@ -15,14 +15,14 @@ def readcsv(file):
 
 def getTnum(gene, csvmatrix):
     temp = []
-    for i in range(1, len(csvmatrix)):
-        if gene in csvmatrix[i][0]: #if gene in the string at csv[i][0], there's more than one name
+    for i in range(len(csvmatrix)):
+        if gene in csvmatrix[i][0]: 
             temp.append(i)
     if len(temp) == 0:
         return('Error: No occurences of gene \'' + gene + '\'.')
         sys.exit(1)
     elif len(temp) == 1:
-        c = temp[0]
+        c = int(temp[0])
     else: 
         return('Error: ' + str(len(temp)) + ' occurences of gene \'' + gene + '\'.')
         sys.exit(1)
@@ -31,43 +31,45 @@ def getTnum(gene, csvmatrix):
 def findP(gene, csvfile, pcutoff, genomelength):
     csvmatrix = readcsv(csvfile)
     position = getTnum(gene, csvmatrix)
+    genenum = len(csvmatrix)
+    print((position -1)%genenum)
     
-    if csvmatrix[position][5] == 'F' and csvmatrix[position-1][5] == 'F':
+    if csvmatrix[position][5] == 'F' and csvmatrix[(position-1)%genenum][5] == 'F':
             #If query and target are more than pcutoff bp away, P is of query
-            if int(csvmatrix[position][3]) > int(csvmatrix[position-1][4])+pcutoff:
+            if int(csvmatrix[position][3]) > (int(csvmatrix[(position-1)%genenum][4])+pcutoff)%GNOMELENGTH:
                 output = csvmatrix[position][3]
             #If are less than pcutoff away, go back until right 
             else:
                 i, counter = 1, 0
                 while i < 100 and counter == 0:
-                    if csvmatrix[position-1-i][5] == 'R':
-                        output = csvmatrix[position-i][3]
+                    if csvmatrix[(position-1-i)%genenum][5] == 'R':
+                        output = csvmatrix[(position-i)%genenum][3]
                         counter = 1
-                    elif int(csvmatrix[position-i][3]) > int(csvmatrix[position - i-1][4])+pcutoff:
-                        output = csvmatrix[position-i][3]
+                    elif int(csvmatrix[(position-i)%genenum][3])%GNOMELENGTH > (int(csvmatrix[(position - i-1)%genenum][4])+pcutoff)%GNOMELENGTH:
+                        output = csvmatrix[(position-i)%genenum][3]
                         counter = 1
                     else:
                         i += 1
         #If both query and target are R
-    elif csvmatrix[position][5] == 'R' and csvmatrix[position + 1][5] == 'R':
-            if int(csvmatrix[position][3]) < int(csvmatrix[position+1][4])-pcutoff:
+    elif csvmatrix[position][5] == 'R' and csvmatrix[(position + 1)%genenum][5] == 'R':
+            if int(csvmatrix[position][3]) < (int(csvmatrix[(position+1)%genenum][4])-pcutoff)%GNOMELENGTH:
                 output = csvmatrix[position][3]
         #If are less than pcutoff away, go forward until right
             else:
                 i, counter = 1, 0
                 while i < 100 and counter == 0:
-                    if csvmatrix[position+i+1][5] == 'F':
-                        output = csvmatrix[position+i][3]
+                    if csvmatrix[(position+i+1)%genenum][5] == 'F':
+                        output = csvmatrix[(position+i)%genenum][3]
                         counter = 1
-                    elif int(csvmatrix[position+i][3]) < int(csvmatrix[position+i+1][4])-pcutoff:
-                        output = csvmatrix[position+i][3]
+                    elif int(csvmatrix[(position+i)%genenum][3]) < (int(csvmatrix[(position+i+1)%genenum][4])-pcutoff)%GNOMELENGTH:
+                        output = csvmatrix[(position+i)%genenum][3]
                         counter = 1
                     else:
                         i += 1
-    elif csvmatrix[position][5] == 'F' and csvmatrix[position-1][5] == 'R':
+    elif csvmatrix[position][5] == 'F' and csvmatrix[(position-1)%genenum][5] == 'R':
         output = csvmatrix[position][3]
 
-    elif csvmatrix[position][5] == 'R' and csvmatrix[position+1][5] == 'F':
+    elif csvmatrix[position][5] == 'R' and csvmatrix[(position+1)%genenum][5] == 'F':
         output = csvmatrix[position][3]
                 
     else:
@@ -78,5 +80,5 @@ def findP(gene, csvfile, pcutoff, genomelength):
                     
 #have to add the addition of the promoter to the end of the csv file       
                 
-x = findP('yaaA', MASTERCSV, INTERGENEDIST, GNOMELENGTH)
+x = findP('yaaB', MASTERCSV, INTERGENEDIST, GNOMELENGTH)
 
